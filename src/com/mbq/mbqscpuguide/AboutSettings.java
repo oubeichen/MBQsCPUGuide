f325acb54daf5a978672f844d996d04e12626dee
Kyler Jeffrey <mbqsniper@hotmail.com>
2013-05-14 11:43:00 -0700

    Testing

--- src/com/mbq/mbqscpuguide/AboutSettings.java
+++ src/com/mbq/mbqscpuguide/AboutSettings.java
@@ -1,8 +1,30 @@
 package com.mbq.mbqscpuguide;
 
-import android.app.Fragment;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
+public class Aboutsettings extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
-public class AboutSettings extends Fragment {
+        //here we set the main layout
+        // this is the parrent layout...here we will add the view we created
+        setContentView(R.layout.main);
 
-	}
+        //call the main layout from xml
+        LinearLayout mainLayout = (LinearLayout)findViewById(R.id.main_layout_id);
+
+        //create a view to inflate the layout_item (the xml with the textView created before)
+        View view = getLayoutInflater().inflate(R.layout.layout_item, mainLayout,false);
+
+        //add the view to the main layout
+        mainLayout.addView(view);
+    }
+}
+ 
