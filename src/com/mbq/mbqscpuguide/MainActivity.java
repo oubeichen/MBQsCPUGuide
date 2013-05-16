1fe37ec7dda0d41da82bc6f33e1a16d2fc41a79d
Kyler Jeffrey <mbqsniper@hotmail.com>
2013-05-16 11:03:12 -0700

    Add more to the lists, and remove the duplicate options in MainActivity

--- src/com/mbq/mbqscpuguide/MainActivity.java
+++ src/com/mbq/mbqscpuguide/MainActivity.java
@@ -15,10 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		menu.add("About");
-		menu.add("Version");
-		menu.add("Readme");		
+		getMenuInflater().inflate(R.menu.main, menu);	
 		return true;
 	}
 
